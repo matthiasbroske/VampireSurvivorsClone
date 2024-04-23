311ba39e9357d73eac0281455d76ddc9ae57fd55
matthiasbroske <tias@broske.com>
2024-04-23 02:32:20 -0500

    utility scene for creating character sets from only characters within string tables

---
+++ Assets/Scripts/Utilities/CharacterSetGenerator.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEditor.Localization;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+using UnityEngine.Localization.Tables;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+namespace Vampire
+{
+    public class CharacterSetGenerator : MonoBehaviour
+    {
+        [SerializeField] private string _characterSetFilePath = "Assets/Fonts/CharacterSet.txt";
+        [SerializeField, TextArea] private string _defaultCharacters = "1234567890-=！@#￥%……&*()~:\"{}[]|\\?/<>,.;'+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ【】「」。：，";
+        [SerializeField] private StringTableCollection[] _stringTables;
+        
+        void Start()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            // Add default characters
+            stringBuilder.Append(_defaultCharacters);
+            // Add every character in every string table
+            foreach (var table in _stringTables)
+            {
+                for (int i = 0; i < LocalizationSettings.AvailableLocales.Locales.Count; ++i)
+                {
+                    var locale = LocalizationSettings.AvailableLocales.Locales[i].Identifier;
+                    stringBuilder.Append(table.GenerateCharacterSet(locale));
+                }
+            }
+            // Save it to character set
+            File.WriteAllText(_characterSetFilePath, stringBuilder.ToString());
+            Debug.Log("Character Set Generated!");
+        }
+    }
+}
